c69d43a1a0f650faf838705fce148ce7f388f773
fan630 <norriswu266@gmail.com>
2020-10-11 21:19:51 +0800

    before Refactor

---
+++ src/components/Victory.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+    <div class="game-victory" v-if="showModel" @click="hideModel">
+        <!-- this is Victory Page -->
+        <button @click="reStart">
+            Restart
+        </button>
+        {{fullName}}
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Victory",
+    data() {
+        return {
+            //showModel: false
+        }
+    },
+    props: {
+        showModel: {
+            type: Boolean,
+            required: false,
+            value: false
+        },
+        name: {
+            type: String,
+        }
+    },
+    computed: {
+        fullName() {
+            return this.name + 'Wu'
+        }
+        // fullName: {
+        //     get() {
+        //         return this.name + 'Wu'
+        //     },
+        //     set(input) {
+        //         // this.fullName = "haha"
+        //     }
+        // }
+    },
+    methods: {
+        hideModel() {
+            // 原本寫法, 注入是單向的, 這是props進來的值
+            // 不應該在組件中改變, 因此改為emit的寫法
+            // this.showModel = false
+            this.$emit('hideModel')
+        },
+        reStart() {
+            this.$emit('startNewGame')
+        },
+
+    }
+}
+</script>
+
+<style lang="scss">
+.game-victory {
+    background-color: rgba(20, 243, 180, 0.6);
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.test {
+    background-color: rgba(20, 243, 180, 0.6);
+}
+</style>
--- src/views/Online.vue
+++ src/views/Online.vue
@@ -16,29 +16,40 @@
             Computer
         </button>
     </div>
+
+    <div>
+        <!-- 模態組件 -->
+        <Victory :name="name" :showModel="showModel" @hideModel="hideModel" @startNewGame="reStart" />
+    </div>
+
 </div>
 </template>
 
 <script>
 import GobangCell from "../components/GobangCell"
+import Victory from "../components/Victory"
 
 export default {
     name: 'Online',
     components: {
-        GobangCell
+        GobangCell,
+        Victory
     },
     data() {
         return {
             gameBoard: [],
             step: [],
             cpuStep: [],
-            BOARD_ROWS :5, 
-            BOARD_COLS :5,
+            BOARD_ROWS: 5,
+            BOARD_COLS: 5,
             leftCells: 0,
             // 有幾個連珠
             wins: 0,
             // 目標連珠數
-            Target:3
+            Target: 3,
+            // 顯示模態框
+            showModel: false,
+            name: 'Jack'
         }
     },
     // computed:{
@@ -46,20 +57,20 @@ export default {
     //        return this.BOARD_ROWS * this.BOARD_COLS
     //     }
     // },
-    watch:{
-      // gameBoard發生改變的時候, 實現偵聽
-      gameBoard(){
-        // 檢定是否為致勝的一步
-        if(this.detectVictory(this.step)){
-          console.log('you win')
-        }else if(this.detectVictory(this.cpuStep)){
-          console.log('computer win')
+    watch: {
+        // gameBoard發生改變的時候, 實現偵聽
+        gameBoard() {
+            // 檢查是否為致勝的一步
+            if (this.detectVictory(this.step)) {
+                this.showModel = true
+            } else if (this.detectVictory(this.cpuStep)) {
+                console.log('computer win')
+            }
         }
-        //this.detectVictory()
-      }
     },
     methods: {
         initBoard() {
+            this.gameBoard.length = 0
             var BOARD_ROWS = 5;
             var BOARD_COLS = 5;
             for (let i = 0; i < BOARD_ROWS; i++) {
@@ -88,6 +99,7 @@ export default {
                 this.leftCells -= 2
             }
         },
+
         onDraw() {
             this.step.splice(2, 1)
             this.cpuStep.splice(2, 1)
@@ -112,34 +124,34 @@ export default {
             } else {
                 this.onCpuPlay()
             }
-        }, 
-        detectVictory(cell){
-          var victory = false
+        },
+        detectVictory(cell) {
+            var victory = false
 
-          // 檢測人是否勝利
-          //var cell = this.step
-          if(this.isRowVictory(cell)){
-             victory = true
-          }
+            // 檢測人是否勝利
+            //var cell = this.step
+            if (this.isRowVictory(cell)) {
+                victory = true
+            }
 
-          if(this.isColumnVictory(cell)){
-             victory = true
-          }
+            if (this.isColumnVictory(cell)) {
+                victory = true
+            }
 
-          if(this.isSlashVictory(cell)){
-             victory = true
-          }
+            if (this.isSlashVictory(cell)) {
+                victory = true
+            }
 
-          if(this.isBackSlashVictory(cell)){
-             victory = true
-          }
+            if (this.isBackSlashVictory(cell)) {
+                victory = true
+            }
 
-          return victory
+            return victory
 
-        }, 
+        },
         // 橫向
-        isRowVictory(cell){
-              // 初始化
+        isRowVictory(cell) {
+            // 初始化
             var victory = false
             this.wins = 0
             //判定
@@ -148,34 +160,34 @@ export default {
             var type = cell[2]
 
             // 首先, 判斷格子的左邊
-            for(var i = col; i >=0; i--){
-                 var item = this.gameBoard[row][i]
-                 if(item[2] && item[2] === type){
-                   this.wins +=1
-                 }else{
-                   break;
-                 }
+            for (var i = col; i >= 0; i--) {
+                var item = this.gameBoard[row][i]
+                if (item[2] && item[2] === type) {
+                    this.wins += 1
+                } else {
+                    break;
+                }
             }
             // 判斷格子的右邊
-            if(col + 1 < this.BOARD_COLS){
-              for(var i = col + 1; i < this.BOARD_COLS; i++){
-                  var item = this.gameBoard[row][i]
-                  if(item[2] && item[2] === type){
-                    this.wins +=1
-                  }else{
-                    break;
-                  }
-              }
+            if (col + 1 < this.BOARD_COLS) {
+                for (var i = col + 1; i < this.BOARD_COLS; i++) {
+                    var item = this.gameBoard[row][i]
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
+                    }
+                }
             }
 
-            if(this.wins >= this.Target){
+            if (this.wins >= this.Target) {
                 victory = true
             }
 
             return victory
-        }, 
+        },
         // 縱項
-        isColumnVictory(cell){
+        isColumnVictory(cell) {
             // 初始化
             var victory = false
             this.wins = 0
@@ -185,45 +197,44 @@ export default {
             var type = cell[2]
 
             // 首先, 判斷格子的上面
-            for(var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++){
-                 var nextRow = row - i 
-                 var nextCol = col
+            for (var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++) {
+                var nextRow = row - i
+                var nextCol = col
 
-                 // 因為是上方判斷,都不能小於0
-                 if(nextRow >=0 && nextCol <this.BOARD_COLS){
+                // 因為是上方判斷,都不能小於0
+                if (nextRow >= 0 && nextCol < this.BOARD_COLS) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
             // 判斷格子的下面
-            for(var i = 1; i < this.Target && i < this.Target; i++){
-                 var nextRow = row + i 
-                 var nextCol = col
+            for (var i = 1; i < this.Target && i < this.Target; i++) {
+                var nextRow = row + i
+                var nextCol = col
 
-                 // 因為是下方判斷,都不能小於0
-                 if(nextRow < this.BOARD_ROWS && nextCol >=0){
+                // 因為是下方判斷,都不能小於0
+                if (nextRow < this.BOARD_ROWS && nextCol >= 0) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
 
-            if(this.wins >= this.Target){
+            if (this.wins >= this.Target) {
                 victory = true
             }
-
 
             return victory
         },
         //斜向
-        isSlashVictory(cell){
+        isSlashVictory(cell) {
             var victory = false
             this.wins = 0
             //判定
@@ -233,45 +244,45 @@ export default {
 
             // 首先, 判斷格子的右上方
             // 避免橫向,縱向 棋盤列數不一樣
-            for(var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++){
-                 var nextRow = row - i 
-                 var nextCol = col + i
+            for (var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++) {
+                var nextRow = row - i
+                var nextCol = col + i
 
-                 // 因為是右上方判斷,都不能小於0
-                 if(nextRow >=0 && nextCol < this.BOARD_COLS){
+                // 因為是右上方判斷,都不能小於0
+                if (nextRow >= 0 && nextCol < this.BOARD_COLS) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
 
             //判斷格子的左下方
-            for(var i = 1; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++){
-                 var nextRow = row + i 
-                 var nextCol = col - i
+            for (var i = 1; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++) {
+                var nextRow = row + i
+                var nextCol = col - i
 
-                 // 因為是左下方判斷,都不能小於0
-                 if(nextRow < this.BOARD_ROWS && nextCol >=0){
+                // 因為是左下方判斷,都不能小於0
+                if (nextRow < this.BOARD_ROWS && nextCol >= 0) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
 
-            if(this.wins >= this.Target){
+            if (this.wins >= this.Target) {
                 victory = true
             }
 
             return victory
         },
         //反斜向 
-        isBackSlashVictory(cell){
+        isBackSlashVictory(cell) {
             var victory = false
             this.wins = 0
             //判定
@@ -281,48 +292,57 @@ export default {
 
             // 首先, 判斷格子的左上方
             // 避免橫向,縱向 棋盤列數不一樣
-            for(var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++){
-                 var nextRow = row - i 
-                 var nextCol = col - i
+            for (var i = 0; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++) {
+                var nextRow = row - i
+                var nextCol = col - i
 
-                 // 因為是左上方判斷,都不能小於0
-                 if(nextRow >=0 && nextCol >=0){
+                // 因為是左上方判斷,都不能小於0
+                if (nextRow >= 0 && nextCol >= 0) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
 
             // 判斷格子的右下方
-            for(var i = 1; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++){
-                 var nextRow = row + i 
-                 var nextCol = col + i
+            for (var i = 1; i < this.BOARD_ROWS && i < this.BOARD_COLS; i++) {
+                var nextRow = row + i
+                var nextCol = col + i
 
-                 // 因為是右下方判斷,都不能小於0
-                 if(nextRow < this.BOARD_ROWS && nextCol < this.BOARD_COLS){
+                // 因為是右下方判斷,都不能小於0
+                if (nextRow < this.BOARD_ROWS && nextCol < this.BOARD_COLS) {
                     var item = this.gameBoard[nextRow][nextCol]
-                    if(item[2] && item[2] === type){
-                      this.wins +=1
-                    }else{
-                      break;
+                    if (item[2] && item[2] === type) {
+                        this.wins += 1
+                    } else {
+                        break;
                     }
-                 }
+                }
             }
 
-            if(this.wins >= this.Target){
+            if (this.wins >= this.Target) {
                 victory = true
             }
 
             return victory
-        }
+        },
+        hideModel() {
+            this.showModel = false
+        },
+        reStart() {
+            this.initBoard();
+        },
+        // showModelHandler(bool) {
+        //     this.showModel = bool
+        // },
     },
     created() {
         this.initBoard()
         this.leftCells = this.BOARD_ROWS * this.BOARD_COLS
-    }, 
+    },
     // updated(){
     //    // 在data任一個屬性發生改變的時候, 就會執行update
     //    this.detectVictory()
